41ab1e23bca5702d7ce414b61079eabcfa4d1cc3
oarutyunyan <devast3d@gmail.com>
2016-09-28 16:29:49 +0300

    fixed main.cpp

--- domeshconverter/Main.cpp
+++ domeshconverter/Main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	options.add_options()
 		("i,infile", "Input file path", cxxopts::value<std::string>())
 		("o,outfile", "Output file path", cxxopts::value<std::string>())
-		("m,mode", "Mode. Available options: m3d_to_json, json_to_m3d", cxxopts::value<std::string>())
+		("m,mode", "Mode. Available options: m3d_to_json, json_to_m3d, m3d_to_obj, json_to_obj", cxxopts::value<std::string>())
 		;
 	options.parse(argc, argv);
 
@@ -35,6 +35,22 @@ int main(int argc, char* argv[])
 		if (result)
 		{
 			result = model.SaveToM3d(outputFile);
+		}
+	}
+	else if (mode == std::string("m3d_to_obj"))
+	{
+		result = Model::LoadFromM3d(inputFile, model);
+		if (result)
+		{
+			result = model.SaveToObj(outputFile);
+		}
+	}
+	else if (mode == std::string("json_to_obj"))
+	{
+		result = Model::LoadFromJson(inputFile, model);
+		if (result)
+		{
+			result = model.SaveToObj(outputFile);
 		}
 	}
 
